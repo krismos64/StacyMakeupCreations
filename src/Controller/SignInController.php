4cf775ec19376481f6911e91ff9f6481dfd0be8e
krismos64 <134715062+krismos64@users.noreply.github.com>
2024-07-10 15:55:26 +0200

    Signin

---
+++ src/Controller/SignInController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\SignInType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SignInController extends AbstractController
+{
+    #[Route('/signin', name: 'app_signin')]
+    public function index(Request $req, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(SignInType::class, $user);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid())
+
+        {
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('signin/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
