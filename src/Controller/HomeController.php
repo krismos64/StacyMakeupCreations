866c0a2a520e9986233c69f75da3ca021dd891fc
krismos64 <134715062+krismos64@users.noreply.github.com>
2024-07-17 14:14:36 +0200

    update home controller and homepage twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Creation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $creations = $entityManager->getRepository(Creation::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'creations' => $creations,
         ]);
     }
 }
